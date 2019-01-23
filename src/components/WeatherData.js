c172838a11774c72f54466a42501618d32b59b20
AngelRdz <ame_mi_amor@hotmail.com>
2019-01-22 20:12:29 -0600

    Validaciones
    - Validaciones con PropTypes.

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -4,6 +4,8 @@ import WeatherTemperature from "./WeatherTemperature";
 
 import { CLOUD } from "../constants/weathers";
 
+import PropTypes from "prop-types";
+
 const WeatherData = () => (
   <div>
     <h2>Weather Data</h2>
@@ -11,5 +13,9 @@ const WeatherData = () => (
     <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
   </div>
 );
+
+Location.propTypes = {
+  city: PropTypes.string.isRequired,
+};
 
 export default WeatherData;
