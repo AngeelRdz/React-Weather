b69027d6e18d847bac09cafcdc230955996caf54
AngelRdz <ame_mi_amor@hotmail.com>
2019-01-22 19:17:06 -0600

    WeatherIcons
    - Agregar icono y uso de función en functional components.

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,11 +1,11 @@
 import React from "react";
-import WeatherExtraInfo from './WeatherExtraInfo';
-import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from "./WeatherExtraInfo";
+import WeatherTemperature from "./WeatherTemperature";
 
 const WeatherData = () => (
   <div>
     <h2>Weather Data</h2>
-    <WeatherTemperature />
+    <WeatherTemperature temperature={20} weatherState={""} />
     <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
   </div>
 );
--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import WeatherIcons from "react-weathericons";
 
-const WeatherTemperature = () => (
-    <div><span>25°</span></div>
+const icons = {
+    sunny: 'day-sunny',
+    fog: 'day-fog'
+} 
+
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+    if (icon) {
+        return <WeatherIcons name={icon} size="2x" />;
+    } else {
+        return <WeatherIcons name={"earthquake"} size="2x" />;
+    }
+    
+}
+
+const WeatherTemperature = ({ temperature, weatherState }) => (
+  <div>
+      {
+          getWeatherIcon(weatherState)
+      }
+    <span>{`${temperature} Cº`}</span>
+  </div>
 );
 
 export default WeatherTemperature;
