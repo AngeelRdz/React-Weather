3b05e8957d1ded1550226885d46c9494a3303237
Angel-at-653633739547 <ame_mi_amor@hotmail.com>
2019-02-18 20:48:16 -0600

    Fetch y solapa de debugging
    - Utilización de variables de retorno Fetch.
    - Transformación de datos.
    - Convert-units (librería instalada).

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
+import convert from 'convert-units';
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 
 import "./styles.css";
 
-import { CLOUD, WINDY } from "./../../constants/weathers";
+import { CLOUD } from "./../../constants/weathers";
+
+const location = "Buenos Aires, ar";
+const api_key = "f777032bfafe7e73f5f4d374915a7678";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`; 
+// const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}&units=metric`; 
 
 const data = {
   temperature: 5,
   weatherState: CLOUD,
   humidity: 10,
   wind: "10 m/s"
-};
-
-const data2 = {
-  temperature: 50,
-  weatherState: WINDY,
-  humidity: 100,
-  wind: "100 m/s"
 };
 
 class WeatherLocation extends Component {
@@ -30,12 +31,43 @@ class WeatherLocation extends Component {
     };
   }
 
+  getTemp = kelvin => {
+    return Number(convert(kelvin).from("K").to("C").toFixed(2));
+  }
+
+  getWeatherState = weather_data => {
+    return CLOUD;
+  }
+
+  getData = weather_data => {
+    const { humidity, temp } = weather_data.main;
+    const { speed } = weather_data.wind;
+    const weatherState = this.getWeatherState(weather_data);
+    const temperature = this.getTemp(temp);
+
+    const data = {
+      humidity,
+      temperature,
+      weatherState,
+      wind: `${speed} m/s`
+    };
+
+    return data;
+
+  }
+
   handleUpdateClick = () => {
-    console.log("actualizado");
-    this.setState({
-      city: 'Ciudad de México',
-      data: data2
-    })
+    fetch(api_weather).then( resolve => {
+      console.log(resolve);
+      return resolve.json();
+    }).then(data => {
+      const newWeather = this.getData(data);
+      console.log(newWeather);
+      this.setState({
+        data: newWeather
+      });
+      console.log(data);
+    });
   }
 
   render() {
